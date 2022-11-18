426248acd020a7ef467f874e33d4a65d3f84f546
almatos <almatos@student.42.fr>
2022-11-18 17:47:11 +0000

    up

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,51 +6,11 @@
 /*   By: almatos <almatos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 11:51:53 by almatos           #+#    #+#             */
-/*   Updated: 2022/11/18 16:01:38 by almatos          ###   ########.fr       */
+/*   Updated: 2022/11/18 16:04:47 by almatos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-int	ft_put_nb(long long n, char *base, int base_size)
-{
-	int				c_count;
-
-	c_count = 0;
-	if (n < 0)
-	{
-		ft_put_c('-');
-		n = n * -1;
-		c_count++;
-	}
-	if (n / base_size > 0)
-		c_count = c_count + ft_put_nb(n / base_size, base, base_size);
-	ft_put_c(base[n % base_size]);
-	c_count++;
-	return (c_count);
-}
-
-int	ft_put_np(unsigned long long n, char *base, int base_size, int frist_exec)
-{
-	int	c_count;
-
-	c_count = 0;
-	if (!n && frist_exec == 0)
-	{
-		write(1, "(nil)", 5);
-		return (5);
-	}
-	if (frist_exec == 0)
-	{
-		ft_put_s("0x");
-		c_count = c_count + 2;
-	}
-	if (n / base_size > 0)
-		c_count = c_count + ft_put_np(n / base_size, base, base_size, 1);
-	ft_put_c(base[n % base_size]);
-	c_count++;
-	return (c_count);
-}
 
 int	ft_check_flags(char str, va_list param)
 {
--- ft_printf/ft_printf_utility.c
+++ ft_printf/ft_printf_utility.c
@@ -6,7 +6,7 @@
 /*   By: almatos <almatos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 18:00:24 by almatos           #+#    #+#             */
-/*   Updated: 2022/11/18 15:13:46 by almatos          ###   ########.fr       */
+/*   Updated: 2022/11/18 16:05:28 by almatos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,4 +34,44 @@ int	ft_put_s(char *s)
 		len++;
 	}
 	return (len);
+}
+
+int	ft_put_nb(long long n, char *base, int base_size)
+{
+	int				c_count;
+
+	c_count = 0;
+	if (n < 0)
+	{
+		ft_put_c('-');
+		n = n * -1;
+		c_count++;
+	}
+	if (n / base_size > 0)
+		c_count = c_count + ft_put_nb(n / base_size, base, base_size);
+	ft_put_c(base[n % base_size]);
+	c_count++;
+	return (c_count);
+}
+
+int	ft_put_np(unsigned long long n, char *base, int base_size, int frist_exec)
+{
+	int	c_count;
+
+	c_count = 0;
+	if (!n && frist_exec == 0)
+	{
+		write(1, "(nil)", 5);
+		return (5);
+	}
+	if (frist_exec == 0)
+	{
+		ft_put_s("0x");
+		c_count = c_count + 2;
+	}
+	if (n / base_size > 0)
+		c_count = c_count + ft_put_np(n / base_size, base, base_size, 1);
+	ft_put_c(base[n % base_size]);
+	c_count++;
+	return (c_count);
 }
