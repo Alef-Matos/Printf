45e74b1b48fa732d8b1fdadd773a127b48ef0fca
almatos <almatos@student.42.fr>
2022-11-21 13:55:18 +0000

    Update 21/11/2022 13:55

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,18 +6,62 @@
 /*   By: almatos <almatos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 11:51:53 by almatos           #+#    #+#             */
-/*   Updated: 2022/11/18 18:41:52 by almatos          ###   ########.fr       */
+/*   Updated: 2022/11/21 13:45:44 by almatos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_check_flags(char str, va_list prm)
+int	ft_put_s(char *s)
+{
+	int	len;
+
+	len = 0;
+	if (!s)
+		return (write(1, "(null)", 6));
+	while (s[len])
+		write(1, &s[len++], 1);
+	return (len);
+}
+
+int	ft_put_b(long long n, char *b, int b_size, int c_count)
+{
+	if (n < 0)
+	{
+		c_count += write(1, "-", 1);
+		n *= -1;
+	}
+	if (n / b_size > 0)
+		c_count += ft_put_b(n / b_size, b, b_size, 0);
+	c_count += write(1, &b[n % b_size], 1);
+	return (c_count);
+}
+
+int	ft_put_p(unsigned long long n, char *b, int b_size, int frist_exec)
+{
+	int	c_count;
+
+	c_count = 0;
+	if (!n && frist_exec == 0)
+		return (write(1, "(nil)", 5));
+	if (frist_exec == 0)
+		c_count += ft_put_s("0x");
+	if (n / b_size > 0)
+		c_count += ft_put_p(n / b_size, b, b_size, 1);
+	write(1, &b[n % b_size], 1);
+	c_count++;
+	return (c_count);
+}
+
+int	ft_check_flags(char str, va_list prm, int c)
 {
 	if (str == '%')
-		return (ft_put_c('%'));
+		return (write(1, "%", 1));
 	else if (str == 'c')
-		return (ft_put_c(va_arg(prm, int)));
+	{
+		c = va_arg(prm, int);
+		return (write(1, &c, 1));
+	}
 	else if (str == 's')
 		return (ft_put_s(va_arg(prm, char *)));
 	else if (str == 'd' || str == 'i')
@@ -43,11 +87,11 @@ int	ft_printf(const char *str, ...)
 	while (*str)
 	{
 		if (*str != '%')
-			c_count += ft_put_c(*str);
+			c_count += write(1, str, 1);
 		else
 		{
 			str++;
-			c_count += ft_check_flags(*str, param);
+			c_count += ft_check_flags(*str, param, 0);
 		}
 		str++;
 	}
--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: almatos <almatos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 10:17:10 by almatos           #+#    #+#             */
-/*   Updated: 2022/11/18 18:48:34 by almatos          ###   ########.fr       */
+/*   Updated: 2022/11/21 13:46:32 by almatos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,8 @@
 # include <unistd.h>
 
 int	ft_printf(const char *format_str, ...);
-int	ft_check_flags(char str, va_list prmts);
+int	ft_check_flags(char str, va_list prm, int c);
 int	ft_put_s(char *s);
-int	ft_put_c(char c);
-int	ft_put_len(const char *s);
 int	ft_put_b(long long n, char *b, int b_size, int c_count);
 int	ft_put_p(unsigned long long n, char *b, int b_size, int frist_exec);
 
